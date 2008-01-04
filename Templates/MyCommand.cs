5222b1ba79d45b3b5e82cc431e2cf4dc941be221
David Siegel djsiegel@gmail.com <Unknown>
2008-01-03 19:56:12 -0600

    Added locate files command. Updated templates.

--- Templates/MyCommand.cs
+++ Templates/MyCommand.cs
@@ -27,6 +27,9 @@ namespace MyAddinNamespace
 		
 		public string Icon {
 			get {
+				// Look in /usr/share/icons/Tango/* for some great icons
+				// to use. Just return the name of the icon, without the
+				// path or extension.
 				return "my-command-icon";
 			}
 		}
@@ -34,32 +37,54 @@ namespace MyAddinNamespace
 		public Type[] SupportedItemTypes {
 			get {
 			return new Type[] {
-					/* Supported item types go here. */
+					// Supported item types go here.
 				};
 			}
 		}
-
-		public Type[] SupportedItemTypes {
-			get {
-			return new Type[] {
-					/* Supported modifier item types go here. */
-				};
-			}
-		}
-
+		
 		public bool SupportsItem (IItem item)
 		{
+			// Here, you can filter the items you support further. The argument
+			// is non-null is a subtype of one of your SupportedItemTypes.
 			return true;
 		}
 		
+		public IItem[] Perform (IItem[] items, IItem[] modItems)
+		{
+			// This is where the magic happens.
+			
+			// If you return a non-null, non-empty array of IItems, they will
+			// be presented to the user in the main window for a new search.
+			// You can return null if you don't want to provide any items to
+			// the user as a result of performing your command.
+			return null;
+		}
+		
+		//////////////////////////////////////////////////////////////////////
+		/// If you don't plan on supporting modifier items (third pane),   ///
+		/// you can safely leave all of the following methods as they are. ///
+		//////////////////////////////////////////////////////////////////////
+
+		public Type[] SupportedModifierItemTypes {
+			get {
+			return new Type[] {
+					// Supported modifier item types go here.
+				};
+			}
+		}
+
+		public bool ModifierItemsOptional {
+			get { return true; }
+		}
+				
 		public bool SupportsModifierItemForItems (IItem[] items, IItem modItem)
 		{
 			return true;
 		}
 		
-		public void Perform (IItem[] items, IItem[] modItems)
+		public IItem[] DynamicModifierItemsForItem (IItem item)
 		{
-			// This is where the magic happens.
+			return null;
 		}
 		
 	}
