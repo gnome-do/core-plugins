436b2013a318d8fba90cab6be74f0c7b0c6bbbd4
Alex Launi <alex.launi@gmail.com>
2008-03-27 18:56:46 -0400

    Fixed comments

--- Vinagre/Vinagre.cs
+++ Vinagre/Vinagre.cs
@@ -1,4 +1,6 @@
-/* AppendTextAction.cs
+/* Vinagre.cs
+ * Authors:
+ * 	Alex Launi <alex.launi@gmail.com>
  *
  * GNOME Do is the legal property of its developers. Please refer to the
  * COPYRIGHT file distributed with this
