b11ad92541b94be05c6039bac50f4a7d0b5dc97e
Alex Launi <alex@eriktorvaldsonn>
2008-05-31 02:16:12 -0400

    Twitter using AbstractLogin

--- Twitter/src/Configuration.cs
+++ Twitter/src/Configuration.cs
@@ -1,35 +1,34 @@
-/*
- * Configuration.cs
- * 
- * GNOME Do is the legal property of its developers, whose names are too numerous
- * to list here.  Please refer to the COPYRIGHT file distributed with this
+/* Configuration2.cs
+ *
+ * GNOME Do is the legal property of its developers. Please refer to the
+ * COPYRIGHT file distributed with this
  * source distribution.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 using System;
 using System.Threading;
-using Gtk;
-using GConf;
 using Do.Addins;
+using Gtk;
 
 namespace DoTwitter
 {
-	public partial class Configuration : Gtk.Bin
-	{		
-		public Configuration()
+	public partial class Configuration : AbstractLogin
+	{
+		public Configuration () : base ()
 		{
 			this.Build();
 			string username, password;
@@ -43,35 +42,34 @@ namespace DoTwitter
 				password = "";
 			}
 			
-			username_entry.Text = username;
-			passwd_entry.Text = password;
-		}
-
-		protected virtual void OnNewAcctClicked (object sender, System.EventArgs e)
-		{
-			Util.Environment.Open ("https://twitter.com/signup");
-		}
-
-		protected virtual void OnApplyBtnClicked (object sender, System.EventArgs e)
-		{
-			string username = username_entry.Text.Trim ();
-			string password = passwd_entry.Text.Trim ();
+			base.Username.Text = username;
+			base.Password.Text = password;
 			
-			apply_btn.Label = "Validating...";
-			apply_btn.Sensitive = false;
+			base.GetAccountLabel.Markup = "<i>Don't have a Twitter account?</i>";
+			base.GetAccountButton.Label = "Get Twitter Account";
+			base.GetAccountButton.Uri = "https://twitter.com/signup";
+			
+		}
+		
+		protected override void Validate ()
+		{
+			string username = base.Username.Text;
+			string password = base.Password.Text;
+			
+			base.StatusLabel.Markup = "Validating...";
+			base.ValidateButton.Sensitive = false;
 			
 			new Thread ((ThreadStart) delegate {
 				bool valid = TwitterAction.TryConnect (username, password);
 				
 				Gtk.Application.Invoke (delegate {
 					if (valid) {
-						valid_lbl.Markup = "<i>Account validation succeeded</i>!";
+						base.StatusLabel.Markup = "<i>Account validation succeeded</i>!";
 						TwitterAction.SetAccountData (username, password);
 					} else {
-						valid_lbl.Markup = "<i>Account validation failed!</i>";
+						base.StatusLabel.Markup = "<i>Account validation failed!</i>";
 					}
-					apply_btn.Label = "Apply";
-					apply_btn.Sensitive = true;
+					base.ValidateButton.Sensitive = true;
 				});
 			}).Start ();
 		}
