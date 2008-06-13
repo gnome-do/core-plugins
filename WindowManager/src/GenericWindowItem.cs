d0b0d0cb2628e6f1be163970f6df11f2508d594f
Jason Smith <jason@ubuntu>
2008-06-13 00:59:03 -0400

    Start cleanup of WindowManager Add riptide plugin.  Downloads dem torrents!

--- WindowManager/src/GenericWindowItem.cs
+++ WindowManager/src/GenericWindowItem.cs
@@ -1,4 +1,4 @@
-// WindowItem.cs
+// GenericWindowItem.cs
 //
 //GNOME Do is the legal property of its developers. Please refer to the
 //COPYRIGHT file distributed with this
@@ -22,51 +22,31 @@
 
 using System;
 using Wnck;
-
 using Do.Universe;
 
 namespace WindowManager
-{	
-	public class WindowItem : IWindowItem
+{
+	public enum GenericWindowType
 	{
-		Wnck.Window window;
-		string icon;
-		
-		public string Name {
-			get {
-				return window.Name;
-			}
-		}
-
-		public string Description {
-			get {
-				return window.Name;
-			}
-		}
-		
-		public Wnck.Window Window {
-			get {
-				return window;
-			}
-		}
-
-		public string Icon {
-			get {
-				return icon;
-			}
-		}
-		
-		public WindowItem(Window w, string icon)
-		{
-			this.window = w;
-			this.icon = icon;
-		}
-		
+		CurrentWindow = 0,
+		PreviousWindow = 1,
+		CurrentApplication = 2,
+		PreviousApplication = 3,
 	}
 	
-	public class ScreenItem : IScreenItem
+	public class GenericWindowItem : IItem
 	{
 		string name, description, icon;
+		GenericWindowType windowType;
+		
+		public GenericWindowItem (string name, string description, string icon, 
+		                          GenericWindowType windowType) 
+		{
+			this.name = name;
+			this.description = description;
+			this.icon = icon;
+			this.windowType = windowType;
+		}
 		
 		public string Name {
 			get {
@@ -86,12 +66,11 @@ namespace WindowManager
 			}
 		}
 
-		
-		public ScreenItem (string name, string description, string icon) 
-		{
-			this.name = name;
-			this.icon = icon;
-			this.description = description;
+		public GenericWindowType WindowType {
+			get {
+				return windowType;
+			}
 		}
+		
 	}
 }
