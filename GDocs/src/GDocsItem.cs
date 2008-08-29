3b94aea0a2b5b7a10bb0571e519727ab220c873c
Alex Launi <alauni@acewks06>
2008-08-29 11:59:23 -0400

    added GDocs plugin

---
+++ GDocs/src/GDocsItem.cs
@@ -0,0 +1,61 @@
+/* GDocsItem.cs
+ *
+ * GNOME Do is the legal property of its developers. Please refer to the
+ * COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Text;
+using Mono.Unix;
+
+using Do.Addins;
+using Do.Universe;
+
+namespace GDocs
+{
+    public class GDocsItem : IOpenableItem
+    {
+        string name, url;
+		
+		public GDocsItem (string name, string url)
+        {
+            this.name = name;
+            this.url = url;
+        }
+
+        public string Name {
+            get { return name; }
+        }
+
+        public string URL {
+            get { return url; }
+        }
+				
+        public virtual string Description {
+            get { return Catalog.GetString ("Google Docs Generic Document"); }
+        }
+
+        public virtual string Icon {
+            get { return "x-office-document"; }
+		}
+		
+		public void Open()
+		{
+			Do.Addins.Util.Environment.Open (url);
+		}
+	}	
+}
---
+++ GDocs/src/GDocsUploadDocument.cs
@@ -0,0 +1,109 @@
+/* GDocsUploadDocument.cs
+ *
+ * GNOME Do is the legal property of its developers. Please refer to the
+ * COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Mono.Unix;
+
+using Do.Addins;
+using Do.Universe;
+
+using Google.GData.Client;
+using Google.GData.Documents;
+
+namespace GDocs
+{
+	public sealed class GDocsUploadDocument : IAction
+	{
+		public string Name {
+			get { return Catalog.GetString ("Upload Document"); }
+		}
+		
+		public string Description {
+			get { return Catalog.GetString ("Upload a document to Google Docs"); }
+        }
+			
+		public string Icon {
+			get { return "gDocsUploadIcon.png@" + GetType ().Assembly.FullName; }
+		}
+		
+		public Type[] SupportedItemTypes {
+			get {
+				return new Type[] {
+					typeof (IFileItem),
+				};
+			}
+		}
+		
+		public Type[] SupportedModifierItemTypes {
+		    get {
+		        return new Type[] {
+		            typeof (ITextItem),
+                };
+            }
+        }
+        
+        public bool ModifierItemsOptional {
+            get {return true; }
+        }
+        
+        public bool SupportsItem (IItem item) 
+        {
+			return IsValidFormat (item as IFileItem);
+        }
+        
+        public bool SupportsModifierItemForItems (IItem[] item, IItem modItem) 
+        {        		
+            return true;
+        }
+        
+        public IItem[] DynamicModifierItemsForItem (IItem item) 
+        {
+            return null;
+        }
+        
+        public IItem[] Perform (IItem[] items, IItem[] modifierItems) 
+        {			
+			string fileName = (items[0] as IFileItem).Path;
+			string documentName = (modifierItems.Length > 0) ? (modifierItems[0] as ITextItem).Text : null;
+			
+            return new IItem [] { GDocs.UploadDocument (fileName, documentName), };
+        }
+		
+		private bool IsValidFormat (IFileItem item)
+		{
+			// Supported uploading format by Google Docs
+			//
+			// Detailed info: http://documents.google.com/support/presentations/bin/answer.py?answer=50092
+			//                http://documents.google.com/support/presentations/bin/answer.py?answer=37603
+			
+			return item.Path.EndsWith(".txt")  || item.Path.EndsWith (".doc") ||
+				    item.Path.EndsWith(".html") || item.Path.EndsWith (".htm") ||
+					item.Path.EndsWith(".odt")  || item.Path.EndsWith (".rtf") ||
+					item.Path.EndsWith(".xls")  || item.Path.EndsWith (".ods") ||
+					item.Path.EndsWith(".csv")  || item.Path.EndsWith (".tsv") ||
+					item.Path.EndsWith(".tsb")  || item.Path.EndsWith (".ppt") ||
+					item.Path.EndsWith(".sxw")  || item.Path.EndsWith (".pps") ||
+					item.Path.EndsWith(".pdf");
+                
+		}
+	}
+}
