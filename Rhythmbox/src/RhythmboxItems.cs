956b4359499e5773a5b5831525959b71be77de83
David Siegel <djsiegel@gmail.com>
2009-02-02 09:29:03 -0800

    Clean Rhythmbox a little.

--- Rhythmbox/src/RhythmboxItems.cs
+++ Rhythmbox/src/RhythmboxItems.cs
@@ -1,8 +1,8 @@
 //  RhythmboxItems.cs
 //
-//  GNOME Do is the legal property of its developers, whose names are too numerous
-//  to list here.  Please refer to the COPYRIGHT file distributed with this
-//  source distribution.
+//  GNOME Do is the legal property of its developers, whose names are too
+//  numerous to list here.  Please refer to the COPYRIGHT file distributed with
+//  this source distribution.
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -18,11 +18,13 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using System.Threading;
 using System.Diagnostics;
+using System.Collections.Generic;
+
+using Mono.Unix;
 
 using Do.Universe;
-using Mono.Unix;
+using Do.Platform;
 
 namespace Do.Rhythmbox
 {
@@ -62,44 +64,49 @@ namespace Do.Rhythmbox
 
 	public class RhythmboxRunnableItem : Item, IRunnableItem
 	{
-		public static readonly RhythmboxRunnableItem[] DefaultItems =
-			new RhythmboxRunnableItem[] {
+		public static readonly IEnumerable<RhythmboxRunnableItem> Items = new [] {
+			new RhythmboxRunnableItem (
+				Catalog.GetString ("Show Current Track"),
+				Catalog.GetString ("Show Notification of Current Track in Rhythmbox"),
+				"gnome-mime-audio",
+				"--notify"),
 
-				new RhythmboxRunnableItem (Catalog.GetString ("Show Current Track"),
-						Catalog.GetString ("Show Notification of Current Track in Rhythmbox"),
-						"gnome-mime-audio",
-						"--notify"),
+			new RhythmboxRunnableItem (
+				Catalog.GetString ("Mute"),
+				Catalog.GetString ("Mute Rhythmbox Playback"),
+				"audio-volume-muted",
+				"--mute"),
 
-				new RhythmboxRunnableItem (Catalog.GetString ("Mute"),
-						Catalog.GetString ("Mute Rhythmbox Playback"),
-						"audio-volume-muted",
-						"--mute"),
+			new RhythmboxRunnableItem (
+				Catalog.GetString ("Unmute"),
+				Catalog.GetString ("Unmute Rhythmbox Playback"),
+				"audio-volume-high",
+				"--unmute"),
 
-				new RhythmboxRunnableItem (Catalog.GetString ("Unmute"),
-						Catalog.GetString ("Unmute Rhythmbox Playback"),
-						"audio-volume-high",
-						"--unmute"),
+			new RhythmboxRunnableItem (
+				Catalog.GetString ("Volume Up"),
+				Catalog.GetString ("Increase Rhythmbox Playback Volume"),
+				"audio-volume-high",
+				"--volume-up"),
 
-				new RhythmboxRunnableItem (Catalog.GetString ("Volume Up"),
-						Catalog.GetString ("Increase Rhythmbox Playback Volume"),
-						"audio-volume-high",
-						"--volume-up"),
-
-				new RhythmboxRunnableItem (Catalog.GetString ("Volume Down"),
-						Catalog.GetString ("Decrease Rhythmbox Playback Volume"),
-						"audio-volume-low",
-						"--volume-down"),
+			new RhythmboxRunnableItem (
+				Catalog.GetString ("Volume Down"),
+				Catalog.GetString ("Decrease Rhythmbox Playback Volume"),
+				"audio-volume-low",
+				"--volume-down"),
 			};
 
-		string name, description, icon, command;
+		string name, description, icon;
 
 		public RhythmboxRunnableItem (string name, string description, string icon, string command)
 		{
 			this.name = name;
 			this.description = description;
 			this.icon = icon;
-			this.command = command;
+			Command = command;
 		}
+
+		string Command { get; set; }
 
 		public override string Name { get { return name; } }
 		public override string Description { get { return description; } }
@@ -107,10 +114,10 @@ namespace Do.Rhythmbox
 
 		public void Run ()
 		{
-			new Thread ((ThreadStart) delegate {
-					Rhythmbox.StartIfNeccessary ();
-					Rhythmbox.Client (command);
-					}).Start ();
+			Services.Application.RunOnThread (() => {
+				Rhythmbox.StartIfNeccessary ();
+				Rhythmbox.Client (Command);
+			});
 		}
 
 	}
