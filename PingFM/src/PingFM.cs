ebad9305121eb59f7340fe208521c1c33a38aa28
Peng Deng <dengpeng@gmail.com>
2009-05-31 20:31:14 +0200

    Add api key link in the prefs gui and clean up the code.

--- PingFM/src/PingFM.cs
+++ PingFM/src/PingFM.cs
@@ -1,21 +1,19 @@
-/* PingFM.cs
- *
- * GNOME Do is the legal property of its developers. Please refer to the
- * COPYRIGHT file distributed with this source distribution.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// PingFM.cs
+// 
+// Copyright (C) 2009 GNOME Do
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Collections.Generic;
@@ -29,12 +27,12 @@ namespace PingFM
 {	
 	public static class PingFM
 	{		
-		static readonly string ConnectionErrorMessage = Catalog.GetString ("Failed to connect to Ping.FM service");		
+		static readonly string ConnectionErrorMessage = Catalog.GetString ("Failed to connect to Ping.FM server.");
 		
 		static PingFMClient client;
 		
 		static PingFM ()
-		{			
+		{
 			Preferences = new PingFMPreferences ();
 			Connect (Preferences.AppKey);
 		}
--- PingFM/src/PingFMClient.cs
+++ PingFM/src/PingFMClient.cs
@@ -1,21 +1,19 @@
-/* PingFMClient.cs
- *
- * GNOME Do is the legal property of its developers. Please refer to the
- * COPYRIGHT file distributed with this source distribution.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// PingFMClient.cs
+// 
+// Copyright (C) 2009 GNOME Do
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Linq;
@@ -65,24 +63,25 @@ namespace PingFM
 			}
 			
 			services.Clear ();
-			services.Add (new PingFMServiceItem (Catalog.GetString ("Microblog"), "pingfm", "microblog", "http://ping.fm", ""));
-			services.Add (new PingFMServiceItem (Catalog.GetString ("Status"), "pingfm", "status", "http://ping.fm", ""));
+			services.Add (new PingFMServiceItem (Catalog.GetString ("Microblog"), 
+			                                     "pingfm", "microblog", "http://ping.fm", ""));
+			services.Add (new PingFMServiceItem (Catalog.GetString ("Status"),
+			                                     "pingfm", "status", "http://ping.fm", ""));
 			
 			// If a service has method "microblog" and/or "status", include it in the service_items list
 			// when both methods are available, use "microblog", because to an individual service
 			// either method is the same, while "microblog" has stricter limitation.			
 			if (sr != null && sr.Status.Equals("OK")) {
 				foreach (PingFMApi.ServiceMethods service in sr.Services) {
-					//string method;
 					if (Regex.IsMatch (service.Methods, @".*(microblog|status).*")) {
-						//method = (service.Methods.Contains ("microblog")) ? "microblog" : "status";
 						services.Add (new PingFMServiceItem (service.Name, service.ID, 
 						                                     service.Methods, service.Url,
 						                                     service.Trigger));
 					}
 				}
 			} else {
-				Log<PingFMClient>.Error (ErrorInMethod, "UpdateServices", Catalog.GetString ("Error occurred in service response"));
+				Log<PingFMClient>.Error (ErrorInMethod, "UpdateServices", 
+				                         Catalog.GetString ("Error occurred in service response"));
 			}
 			Log<PingFMClient>.Debug ("Retrieved {0} Ping.FM services", services.Capacity);
 		}
@@ -115,9 +114,11 @@ namespace PingFM
 		Notification GetSuccessfulNotification (string service, string method, string icon)
 		{
 			if (service != null)
-				return new Notification (PostSuccessTitle, String.Format (SinglePostSuccess, method, service), icon);
+				return new Notification (PostSuccessTitle, 
+				                         String.Format (SinglePostSuccess, method, service), icon);
 			else
-				return new Notification (PostSuccessTitle, String.Format (MultiPostSuccess, method), icon);
+				return new Notification (PostSuccessTitle, 
+				                         String.Format (MultiPostSuccess, method), icon);
 		}
 		
 		Notification GetFailedNotification (string icon)
