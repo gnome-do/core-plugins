ac7167eee17dcfce5afd61e1e18784bd2838e2fc
Jonathan Lambrechts <jonathan.lambrechts@gmail.com>
2009-07-25 00:17:38 +0200

    Trying to integrate monodevelop and follow guidelines

--- Chromium/src/ChromiumBookmarkItemSource.cs
+++ Chromium/src/ChromiumBookmarkItemSource.cs
@@ -18,8 +18,8 @@
  * along with this program.	If not, see <http://www.gnu.org/licenses/>.
  */
 using System;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 
 using Mono.Addins;
@@ -28,34 +28,34 @@ using Do.Platform;
 using Do.Universe;
 using Do.Universe.Common;
 
-namespace Chromium
-{
-	public class ChromiumBookmarkItemSource : ItemSource 
-	{
-		
+namespace Chromium {
+	
+	public class ChromiumBookmarkItemSource : ItemSource {
+	
 		List<Item> items;
 		
-		public ChromiumBookmarkItemSource()
+		public ChromiumBookmarkItemSource ()
 		{
 			items = new List<Item> ();
 		}
 		
-		public override string Name { 
-			get { return AddinManager.CurrentLocalizer.GetString ("Chromium Bookmarks"); } 
-		}
-
-		public override string Description { 
-			get { return AddinManager.CurrentLocalizer.GetString ("Indexes your Chromium bookmarks"); } 
-		}
-
-		public override string Icon { 
-			get { return "chromium-browser"; } 
+		public override string Name {
+			get { return AddinManager.CurrentLocalizer.GetString ("Chromium Bookmarks"); }
 		}
 		
-		public override	IEnumerable<Type> SupportedItemTypes {
+		public override string Description {
+			get { return AddinManager.CurrentLocalizer.GetString ("Indexes your Chromium bookmarks"); }
+		}
+		
+		public override string Icon {
+			get { return "chromium-browser"; }
+		}
+		
+		public override IEnumerable<Type> SupportedItemTypes {
 			get { yield return typeof (BookmarkItem); }
 		}
-		public override IEnumerable<Item> Items	{
+		
+		public override IEnumerable<Item> Items {
 			get { return items; }
 		}
 		
@@ -64,28 +64,29 @@ namespace Chromium
 			string home = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
 			string bookmarksFile = "~/.config/chromium/Default/Bookmarks".Replace ("~", home);
 			try {
-				FileStream fs = new FileStream(bookmarksFile,FileMode.Open,FileAccess.Read);
-				StreamReader reader = new StreamReader(fs);
-				Regex RE = new Regex("(\"([^\"]*)\" *: *\"([^\"]*)\")|[{}]", RegexOptions.Multiline);
-				String type="",name="",url="";
-				items.Clear();
-				foreach(Match m in RE.Matches(reader.ReadToEnd()))
-				{
-					if(m.Value=="{"){
-						type="";
-						name="";
-						url="";
+				string type = "";
+				string name = "";
+				string url = "";
+				items.Clear ();
+				Regex RE = new Regex ("(\"([^\"]*)\" *: *\"([^\"]*)\")|[{}]", RegexOptions.Multiline);
+				FileStream fs = new FileStream (bookmarksFile, FileMode.Open,FileAccess.Read);
+				StreamReader reader = new StreamReader (fs);
+				foreach (Match m in RE.Matches (reader.ReadToEnd ())) {
+					if (m.Value == "{") {
+						type = "";
+						name = "";
+						url = "";
 					}
-					if(m.Value=="}" && type=="url"){
-						items.Add (new BookmarkItem (name, url));						
+					if (m.Value == "}" && type == "url" && name != "" && url != "") {
+						items.Add (new BookmarkItem (name, url));
 					}
-					if(m.Value.StartsWith("\"")){
-						if(m.Groups[2].Value=="url")
-							url=m.Groups[3].Value;
-						if(m.Groups[2].Value=="name")
-							name=m.Groups[3].Value;
-						if(m.Groups[2].Value=="type")
-							type=m.Groups[3].Value;
+					if (m.Value.StartsWith("\"")) {
+						if (m.Groups[2].Value == "url")
+							url = m.Groups[3].Value;
+						if (m.Groups[2].Value == "name")
+							name = m.Groups[3].Value;
+						if (m.Groups[2].Value == "type")
+							type = m.Groups[3].Value;
 					}
 				}
 			}
