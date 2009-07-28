d361a15a7f0f5d5e95d279bde86cad2897a2a4b0
Jonathan Lambrechts <jonathan.lambrechts@gmail.com>
2009-07-28 15:02:51 +0200

    handle utf8 escaped character in bookmarks name

--- Chromium/src/ChromiumBookmarkItemSource.cs
+++ Chromium/src/ChromiumBookmarkItemSource.cs
@@ -20,6 +20,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Globalization;
 using System.Text.RegularExpressions;
 
 using Mono.Addins;
@@ -59,6 +60,15 @@ namespace Chromium
 			get { return items; }
 		}
 		
+		static string UnescapeUTF8 (string s) {
+			foreach (Match m in Regex.Matches (s, @"\\u([0-9A-F]{4})")) {
+				char c = (char) int.Parse (m.Groups[1].Value, NumberStyles.HexNumber);
+				s = s.Replace (m.Groups[0].Value, c.ToString());
+			}
+			return s;
+		}
+		
+		
 		public override void UpdateItems ()
 		{
 			string type = "", name = "", url = "";
@@ -66,7 +76,7 @@ namespace Chromium
 			string bookmarksFile = "~/.config/chromium/Default/Bookmarks".Replace ("~", home);
 			try {				
 				Regex RE = new Regex ("(\"([^\"]*)\" *: *\"([^\"]*)\")|[{}]", RegexOptions.Multiline);
-				FileStream fs = new FileStream (bookmarksFile, FileMode.Open,FileAccess.Read);
+				FileStream fs = new FileStream (bookmarksFile, FileMode.Open, FileAccess.Read);
 				StreamReader reader = new StreamReader (fs);
 				
 				items.Clear ();
@@ -78,7 +88,7 @@ namespace Chromium
 						name = "";
 					}
 					else if (m.Value == "}" && type == "url" && !string.IsNullOrEmpty (name) && !string.IsNullOrEmpty (url)) {
-						items.Add (new BookmarkItem (name, url));
+						items.Add (new BookmarkItem (UnescapeUTF8(name), url));
 					}
 					else if (m.Value.StartsWith ("\"")) {
 						if (m.Groups [2].Value == "url")
