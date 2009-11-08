51fee19185177a24cb25524ab980b2f474fa4bcb
Marco Antonio Villegas Vega <marvil07@gmail.com>
2009-11-08 18:58:01 -0500

    Add GNOME-Calculator, a simple pipe to gcalctool

---
+++ GNOME-Calculator/src/GCalcToolAction.cs
@@ -0,0 +1,70 @@
+/**
+ * GNOME Do is the legal property of its developers. Please refer to the
+ * COPYRIGHT file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.IO;
+using System.Web;
+using System.Net;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+using Mono.Addins;
+
+using Do.Universe;
+using Do.Universe.Common;
+
+namespace GNOME.GCalcTool {
+
+    public class GCalcToolAction : Act {
+
+        public GCalcToolAction() {
+        }
+
+        public override string Name {
+            get { return "GNOME Calculator";  }
+        }
+
+        public override string Description {
+            get { return AddinManager.CurrentLocalizer.GetString ("Use GNOME Calculator to make simple calculations."); }
+        }
+
+        public override string Icon {
+            get { return "accessories-calculator"; }
+        }
+
+        public override IEnumerable<Type> SupportedItemTypes {
+            get { yield return typeof (ITextItem); }
+        }
+
+        public override IEnumerable<Item> Perform (IEnumerable<Item> items, IEnumerable<Item> modItems) {
+            string expression = (items.First () as ITextItem).Text;
+            string result = "";
+
+            ProcessStartInfo ps = new ProcessStartInfo ("gcalctool", "-s " + expression);
+            ps.UseShellExecute = false;
+            ps.RedirectStandardOutput = true;
+            Process p = Process.Start (ps);
+
+            result = p.StandardOutput.ReadToEnd ();
+            p.WaitForExit ();
+
+            yield return new TextItem (result);
+        }
+    }
+}
