d6f385d30c86f0f69e11cae34bb5fffa87e26463
Mikhail Trishchenkov <kriomant@gmail.com>
2010-07-11 14:11:41 +0700

    Refactor out common code of speed limit actions

--- Transmission/src/TorrentLimitDownloadSpeedAction.cs
+++ Transmission/src/TorrentLimitDownloadSpeedAction.cs
@@ -10,11 +10,8 @@ using Do.Platform;
 using Do.Universe;
 
 namespace Transmission {
-	
-	public class TorrentLimitDownloadSpeedAction: Act {
 
-		public TorrentLimitDownloadSpeedAction() {
-		}
+	public class TorrentLimitDownloadSpeedAction: TorrentAbstractLimitSpeedAction {
 
 	    public override string Name {
 			get { return AddinManager.CurrentLocalizer.GetString("Limit download speed"); }
@@ -28,59 +25,21 @@ namespace Transmission {
 			get { return "top"; }
 		}
 
-		public override IEnumerable<Type> SupportedItemTypes {
-			get { yield return typeof (TorrentItem); }
+		protected override PredefinedSpeed GetCurrentSpeedItem(TorrentItem torrent) {
+			int currentSpeed = torrent.DownloadSpeedLimit;
+			return new PredefinedSpeed(
+				currentSpeed,
+				string.Format("Saved: {0}", Utils.FormatSpeed(currentSpeed)),
+				"Use limit from torrent settings"
+			);
 		}
 
-		public override IEnumerable<Type> SupportedModifierItemTypes {
-			get {
-				yield return typeof (ITextItem);
-				yield return typeof (PredefinedSpeed);
-			}
+		protected override void SetSpeedLimit(TransmissionAPI api, TorrentItem torrent, int speed) {
+			bool limit_speed = (speed != 0);
+			int? limit = (speed == 0 ? (int?)null : speed);
+			api.SetTorrents(new string[] {torrent.HashString}, null, limit_speed, limit, null, null);
+			torrent.DownloadSpeedLimit = speed;
 		}
 
-		public override bool ModifierItemsOptional {
-			get { return false; }
-		}
-
-		public override IEnumerable<Item> DynamicModifierItemsForItem(Item item) {
-			TorrentItem torrent = (TorrentItem)item;
-			int current = torrent.DownloadSpeedLimit;
-
-			yield return new PredefinedSpeed(0, "Unlimited", "Turn download speed limit off");
-			yield return new PredefinedSpeed(current, string.Format("Saved: {0}", Utils.FormatSpeed(current)), "Use limit from torrent settings");
-			foreach (PredefinedSpeed speed in Utils.PredefinedSpeedItems)
-				yield return speed;
-		}
-
-		public override IEnumerable<Item> Perform(IEnumerable<Item> items, IEnumerable<Item> modItems) {
-			TorrentItem item = items.First() as TorrentItem;
-			string speed_str = (modItems.First() as ITextItem).Text;
-
-			int? speed = null;
-			try {
-				// Try to parse entered speed value.
-				speed = Utils.ParseSpeed(speed_str);
-
-			} catch (ArgumentException) {
-				Log<TransmissionPlugin>.Debug("Invalid speed string: {0}", speed_str);
-
-				// Show notification about invalid speed value with some hints on
-				// accepted formats.
-				string message = AddinManager.CurrentLocalizer.GetString(
-					"Can't recognize \"{0}\" as speed\nUse values like: 100k, 50 kb, 20m, 10 mib"
-				);
-				Services.Notifications.Notify("Transmission", string.Format(message, speed_str), "transmission");
-			}
-
-			// If speed is recognized successfully, set speed limit and update item.
-			if (speed.HasValue) {
-				TransmissionAPI api = TransmissionPlugin.getTransmission();
-				api.SetTorrents(new string[] {item.HashString}, null, true, speed.Value, null, null);
-				item.DownloadSpeedLimit = speed.Value;
-			}
-
-			yield break;
-		}
 	}
 }
--- Transmission/src/TorrentLimitUploadSpeedAction.cs
+++ Transmission/src/TorrentLimitUploadSpeedAction.cs
@@ -10,11 +10,8 @@ using Do.Platform;
 using Do.Universe;
 
 namespace Transmission {
-	
-	public class TorrentLimitUploadSpeedAction: Act {
 
-		public TorrentLimitUploadSpeedAction() {
-		}
+	public class TorrentLimitUploadSpeedAction: TorrentAbstractLimitSpeedAction {
 
 	    public override string Name {
 			get { return AddinManager.CurrentLocalizer.GetString ("Limit upload speed"); }
@@ -28,58 +25,21 @@ namespace Transmission {
 			get { return "top"; }
 		}
 
-		public override IEnumerable<Type> SupportedItemTypes {
-			get { yield return typeof (TorrentItem); }
+		protected override PredefinedSpeed GetCurrentSpeedItem(TorrentItem torrent) {
+			int currentSpeed = torrent.UploadSpeedLimit;
+			return new PredefinedSpeed(
+				currentSpeed,
+				string.Format("Saved: {0}", Utils.FormatSpeed(currentSpeed)),
+				"Use limit from torrent settings"
+			);
 		}
 
-		public override IEnumerable<Type> SupportedModifierItemTypes {
-			get {
-				yield return typeof (ITextItem);
-				yield return typeof (PredefinedSpeed);
-			}
+		protected override void SetSpeedLimit(TransmissionAPI api, TorrentItem torrent, int speed) {
+			bool limit_speed = (speed != 0);
+			int? limit = (speed == 0 ? (int?)null : speed);
+			api.SetTorrents(new string[] {torrent.HashString}, null, null, null, limit_speed, limit);
+			torrent.UploadSpeedLimit = speed;
 		}
 
-		public override bool ModifierItemsOptional {
-			get { return false; }
-		}
-
-		public override IEnumerable<Item> DynamicModifierItemsForItem(Item item) {
-			TorrentItem torrent = (TorrentItem)item;
-			int current = torrent.UploadSpeedLimit;
-
-			yield return new PredefinedSpeed(0, "Unlimited", "Turn upload speed limit off");
-			yield return new PredefinedSpeed(current, string.Format("Saved: {0}", Utils.FormatSpeed(current)), "Use limit from torrent settings");
-			foreach (PredefinedSpeed speed in Utils.PredefinedSpeedItems)
-				yield return speed;
-		}
-
-		public override IEnumerable<Item> Perform(IEnumerable<Item> items, IEnumerable<Item> modItems) {
-			TorrentItem item = items.First() as TorrentItem;
-			string speed_str = (modItems.First() as ITextItem).Text;
-
-			int? speed = null;
-			try {
-				// Try to parse entered speed value.
-				speed = Utils.ParseSpeed(speed_str);
-
-			} catch (ArgumentException) {
-				Log<TorrentLimitUploadSpeedAction>.Debug("Invalid speed string: {0}", speed_str);
-
-				// Show notification about invalid speed value with some hints on
-				// accepted formats.
-				string message = AddinManager.CurrentLocalizer.GetString(
-					"Can't recognize \"{0}\" as speed\nUse values like: 100k, 50 kb, 20m, 10 mib"
-				);
-				Services.Notifications.Notify("Transmission", string.Format(message, speed_str), "transmission");
-			}
-
-			if (speed.HasValue) {
-				TransmissionAPI api = TransmissionPlugin.getTransmission();
-				api.SetTorrents(new string[] {item.HashString}, null, true, null, null, speed.Value);
-				item.UploadSpeedLimit = speed.Value;
-			}
-
-			yield break;
-		}
 	}
 }
